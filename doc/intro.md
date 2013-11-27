67757d8ab333b1ac973e8a93c78ad17ffdbcaa55
Shriphani Palakodety <shriphanip@gmail.com>
2013-11-27 15:44:49 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj_dimension
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj_dimension "0.1.0-SNAPSHOT"
+  :description "A collection of dimension reduction algorithms"
+  :url "http://blog.shriphani.com/"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [incanter "1.5.4"]])
